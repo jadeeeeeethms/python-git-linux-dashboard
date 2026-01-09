f57e133a94fea36effc6e5962b1b5869d80499c1
Ubuntu <ubuntu@ip-172-31-4-186.eu-north-1.compute.internal>
2026-01-09 16:04:02 +0000

    remove streamlit from finnhub backend

--- data/finnhub_quote.py
+++ data/finnhub_quote.py
@@ -1,17 +1,18 @@
-import requests
-import streamlit as st
 import os
+import requests
 
-@st.cache_data(ttl=300)
 def fetch_quote(symbol):
-
     api_key = os.getenv("FINNHUB_API_KEY")
 
     if not api_key:
         raise RuntimeError("FINNHUB_API_KEY not found")
 
     url = "https://finnhub.io/api/v1/quote"
-    r = requests.get(url, params={"symbol": symbol, "token": api_key}, timeout=10)
+    r = requests.get(
+        url,
+        params={"symbol": symbol, "token": api_key},
+        timeout=10
+    )
 
     r.raise_for_status()
     data = r.json()
